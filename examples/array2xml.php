fdc7ae44962353689cd632c074d39af9895e4b67
Steve King <steve@sming.com.au>
2011-06-21 20:01:43 +1000

    added some comments

--- examples/array2xml.php
+++ examples/array2xml.php
@@ -1,14 +1,44 @@
 <?php 
+	/**
+	 * pushOutputXML Class
+	 * @author Steve King <steve@stevenking.com.au>
+	 *
+	 * This is a basic example of using method overloading
+	 * It's dirty and I wouldn't use it in production.
+	 * 
+	 */
 	class pushOutputXML {
 		
+		/**
+		 * Coming into the class, this is where we'd store the
+		 * array that's passed.
+		 * @var array
+		 */
 		private $_inputObject;
 		
+		/**
+		 * Heading out of the class, this is what we'd pull from
+		 * outside in order to get our XML. Returns simplexml object
+		 * @var object
+		 */
+		public $_outputObject;
+		
+		/**
+		 * Our constructor, allows us to pull in an object and begin
+		 * work on generating the XML. Builds our base level simplexml
+		 * object too;
+		 * @param array $inputObject
+		 */
 		function __construct($inputObject) {
 			$this->_inputObject = $inputObject;
-			$this->_outputObject = simplexml_load_string('<fas></fas>');
+			$this->_outputObject = simplexml_load_string('<output></output>');
 			$this->_matchOutput();
 		}
 	
+		/**
+		 * Detects and matches an array from input, then processes the top level
+		 * items;
+		 */
 		private function _matchOutput() {
 			if ( isset($this->_inputObject) && is_array($this->_inputObject) ) {
 				foreach ( array_keys($this->_inputObject) as $theInput) {
@@ -17,13 +47,17 @@
 			}
 		}
 		
+		/**
+		 * Crazy Method
+		 * @param mixed $Method
+		 * @param array $Args
+		 */
 		private function __call($Method,$Args) {
 			if ($Method != '_matchOutput' ) {
 				if ( !isset($Args[1]) ) {
 					$iterationXML = $this->_outputObject->addChild($Method);
 				}
 				else {
-				
 					if ( is_array($Args[0]) ) {
 						$iterationXML = $Args[1]->addChild($Method);
 						// Watchoo - push that in.
